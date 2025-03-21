590d3d9e15351cb3f02433af1284db499d9e54c3
mervymulatillo <mervymulatillo@gmail.com>
2025-03-20 21:35:46 -0500

    Actualización en estructuradedatos.py

--- estructuradedatos.py
+++ estructuradedatos.py
@@ -1,4 +1,4 @@
-print("--------------------------------------------bienvenidos a mis estructura de datos--------------")
+print("------------------------------bienvenidos a mi estructura de datos--------------")
 # Listas, tuplas, conjuntos y diccionarios
 
 #Variable simple
@@ -9,10 +9,52 @@ x - 18
 
 
 # Estructura de datos
-#lista
+
+#1.- Lista
 numeros = [1,2,3,4,5,6,'MervyMulatillo',True,10,11,12]
+# Las listas son miutavles
+# si permiten valores duplicados
+print(numeros)
+print(numero[6])
 
-# Las listas son mutables
+#2.- Tuplas
+numeros_primeros=(1,2,3,4,5,6,'MervyMulatillo',True,10,11,12)
+# Las tuplas son inmutables
+# si permiten valores duplicados
+print(numeros_primeros)
 
 
-# Las listas son inmutables
+#3. Conjuntos
+numeros_nuevos =(1,2,3,4,5,6,'MervyMulatillo',True,10,11,12)
+print(numeros_nuevos)
+
+
+#4. Diccionarios
+persona = {
+    "Nombre" : "Mervy Mulatillo",
+    "Edad": "29",
+    "Ciudad": "Piura",
+    "Correo": "mervymulatillo@gmail.com"
+}
+print(persona)
+
+
+# Listas de personas
+persona1 = {
+    "Nombre" : "Mervy Mulatillo",
+    "Edad": "29",
+    "Ciudad": "Piura",
+    "Correo": "mervymulatillo@gmail.com"
+}
+persona2 = {
+    "Nombre" : "Mervy Mulatillo",
+    "Edad": "30",
+    "Ciudad": "Piura",
+    "Correo": "mervymulatillo@gmail.com"
+}
+persona3 = {
+    "Nombre" : "Mervy Mulatillo",
+    "Edad": "31",
+    "Ciudad": "Piura",
+    "Correo": "mervymulatillo@gmail.com"
+}
