b1c3f03ebc2a319c911db7ac9ff82fd95b86c133
mervymulatillo <mervymulatillo@gmail.com>
2025-03-20 21:04:51 -0500

    Actualización de estructuradedatos.py

---
+++ estructuradedatos.py
@@ -0,0 +1,18 @@
+print("--------------------------------------------bienvenidos a mis estructura de datos--------------")
+# Listas, tuplas, conjuntos y diccionarios
+
+#Variable simple
+x=9
+y=10
+tem -0
+x - 18
+
+
+# Estructura de datos
+#lista
+numeros = [1,2,3,4,5,6,'MervyMulatillo',True,10,11,12]
+
+# Las listas son mutables
+
+
+# Las listas son inmutables
